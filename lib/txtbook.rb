3292a27cf315b74a635b9253714d0b73e8b80e99
Ben Rady <benrady@gmail.com>
2009-08-17 08:39:53 -0500

    Can create an empty textbook using txtbook command

--- lib/txtbook.rb
+++ lib/txtbook.rb
@@ -1,4 +1,18 @@
+require 'fileutils'
+
 module TxtBook
+  TEXTBOOK_DIRS = ['answers', 'exercises', 'slides']
+  
+  class Factory
+    def create(cmd_line_args)
+      textbook_root = cmd_line_args[0]
+      TEXTBOOK_DIRS.each do |dir|
+        FileUtils.mkdir_p File.join(textbook_root, dir)
+      end
+      
+    end
+  end
+  
   class Tools
     
   end
--- spec/txtbook_spec.rb
+++ spec/txtbook_spec.rb
@@ -1,7 +1,22 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require 'ftools'
+require 'fileutils'
 
 module TxtBook
-  describe Tools do
+  describe Factory do    
+    it "can create a new TextBook" do
+      # DEBT It would be nice if we could mock out the file system module
+      # but i'm not sure how to do that. 
+      factory = Factory.new
+      factory.create(['new-book'])
+      File.directory?('new-book').should be_true
+      File.directory?('new-book/answers').should be_true
+      File.directory?('new-book/exercises').should be_true
+      File.directory?('new-book/slides').should be_true
+    end
     
+    after(:each) do
+      FileUtils.rm_rf('new-book')
+    end
   end
 end
